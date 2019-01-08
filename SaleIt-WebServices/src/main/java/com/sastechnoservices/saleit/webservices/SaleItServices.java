cb9a502a799e40d0f690b0c7aa39937d3b921211
Abhijeet Guru <abhijeetguru050@gmail.com>
2019-01-08 22:50:47 +0530

    Service in json format exposed.

--- SaleIt-WebServices/src/main/java/com/sastechnoservices/saleit/webservices/SaleItServices.java
+++ SaleIt-WebServices/src/main/java/com/sastechnoservices/saleit/webservices/SaleItServices.java
@@ -2,6 +2,7 @@ package com.sastechnoservices.saleit.webservices;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -17,10 +18,10 @@ public interface SaleItServices {
 	String testServices();
 	
 	
-	@GET
+	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Path("/addItem")
-	AddItemtoCartResponse addItemtoCart(AddItemtoCartRequest addItemtoCartRequest);;
+	AddItemtoCartResponse addItemtoCart(AddItemtoCartRequest addItemtoCartRequest);
 
 }
