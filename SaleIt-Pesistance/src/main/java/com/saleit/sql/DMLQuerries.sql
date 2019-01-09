6219004b48014a33a5e19ca693c178a1eedcb36e
Sudeep <Sudeep@SUDEEP>
2019-01-09 21:35:04 +0530

    rest updated@Sudeep

---
+++ SaleIt-Pesistance/src/main/java/com/saleit/sql/DMLQuerries.sql
@@ -0,0 +1,270 @@
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('100', 'Ravi', '9876541523', 'ravi@gmail.com', '<com.saleit.domains.Address>
+  <addressLine1>Ashok Nagar</addressLine1>
+  <addressLine2>Near Sangam Talkies</addressLine2>
+  <city>Cutack</city>
+  <pin>751264</pin>
+  <landMark>Sangam Talkies</landMark>
+</com.saleit.domains.Address>','abcdef','SC');
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('101', 'Aakash', '9658745862', 'aakash@gmail.com', '<com.saleit.domains.Address>
+  <addressLine1>KharBela Nagar</addressLine1>
+  <addressLine2>Near Tarini Temple</addressLine2>
+  <city>Cutack</city>
+  <pin>751784</pin>
+  <landMark>Tarini Temple</landMark>
+</com.saleit.domains.Address>','bjhhjasd','SC');
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('102', 'Souvik', '9658741256', 'souvik@gmail.com', '<com.saleit.domains.Address>
+  <addressLine1>Tamrit Colony</addressLine1>
+  <addressLine2>Near Ramco Colege</addressLine2>
+  <city>Angul</city>
+  <pin>759122</pin>
+  <landMark>Ramco Colege</landMark>
+</com.saleit.domains.Address>','qweqwqwwq','SC');
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('103', 'Jayant', '8759654855', 'jayant@gmail.com', '<com.saleit.domains.Address>
+  <addressLine1>Railway Colony</addressLine1>
+  <addressLine2>Near Railway Station</addressLine2>
+  <city>Dhenkanal</city>
+  <pin>759253</pin>
+  <landMark>Railway Station</landMark>
+</com.saleit.domains.Address>','asdasdas','SC');
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('104', 'Sachin', '9875862541', 'sachin@gmail.com', '<com.saleit.domains.Address>
+  <addressLine1>Nandan Vihar</addressLine1>
+  <addressLine2>Near Subhadra Appartment</addressLine2>
+  <city>Bhubaneswar</city>
+  <pin>751024</pin>
+  <landMark>Subhadra Appartment</landMark>
+</com.saleit.domains.Address>','asdasdasdas','SC');
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('105', 'Debasis', '8796512588', 'debasis@gmail.com', '<com.saleit.domains.Address>
+  <addressLine1>Nandan Vihar</addressLine1>
+  <addressLine2>Near Subhadra Appartment</addressLine2>
+  <city>Bhubaneswar</city>
+  <pin>751024</pin>
+  <landMark>Subhadra Appartment</landMark>
+</com.saleit.domains.Address>','asasdas','C');
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('106', 'Pintu', '9874621555', 'pintu@gmail.com', '<com.saleit.domains.Address>
+  <addressLine1>Railway Colony</addressLine1>
+  <addressLine2>Near Railway Station</addressLine2>
+  <city>Dhenkanal</city>
+  <pin>759253</pin>
+  <landMark>Railway Station</landMark>
+</com.saleit.domains.Address>','sadasdas','C');
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('107', 'Papu', '9874585633', 'USA', '<com.saleit.domains.Address>
+  <addressLine1>Railway Colony</addressLine1>
+  <addressLine2>Near Railway Station</addressLine2>
+  <city>Dhenkanal</city>
+  <pin>759253</pin>
+  <landMark>Railway Station</landMark>
+</com.saleit.domains.Address>','asdasdasdas','C');
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('108', 'Sarad', '9875662544', 'sarad@gmail.com', '<com.saleit.domains.Address>
+  <addressLine1>Nandan Vihar</addressLine1>
+  <addressLine2>Near Subhadra Appartment</addressLine2>
+  <city>Bhubaneswar</city>
+  <pin>751024</pin>
+  <landMark>Subhadra Appartment</landMark>
+</com.saleit.domains.Address','asdasasdasdas','C');
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('109', 'Niel', '9874586259', 'niel@gmail.com', '<com.saleit.domains.Address>
+  <addressLine1>KharBela Nagar</addressLine1>
+  <addressLine2>Near Tarini Temple</addressLine2>
+  <city>Cutack</city>
+  <pin>751784</pin>
+  <landMark>Tarini Temple</landMark>
+</com.saleit.domains.Address>','asdasdasdas','C');
+INSERT INTO rtl_user (user_id, user_name, user_contact_number, user_email_address, user_address,password,usertype)
+VALUES ('110', 'Badal', '9658744863', 'badal@gmail.com', '<com.saleit.domains.Address>
+  <addressLine1>Nandan Vihar</addressLine1>
+  <addressLine2>Near Subhadra Appartment</addressLine2>
+  <city>Bhubaneswar</city>
+  <pin>751024</pin>
+  <landMark>Subhadra Appartment</landMark>
+</com.saleit.domains.Address>','asdasdasda','C');
+
+
+
+
+INSERT INTO rtl_shop (shop_id, shop_name, shop_address, contact_number, email_address,shop_type,shop_owner_id)
+VALUES ('10001', 'Odisha Food Stall', '<com.saleit.domains.Address>
+  <addressLine1>Ashok Nagar</addressLine1>
+  <addressLine2>Near Sangam Talkies</addressLine2>
+  <city>Cutack</city>
+  <pin>751264</pin>
+  <landMark>Sangam Talkies</landMark>
+</com.saleit.domains.Address>', '9658785632', 'odishafood@gmail.com','Food','100');
+
+INSERT INTO rtl_shop (shop_id, shop_name, shop_address, contact_number, email_address,shop_type,shop_owner_id)
+VALUES ('10002', 'Rambo Fast Food', '<com.saleit.domains.Address>
+  <addressLine1>KharBela Nagar</addressLine1>
+  <addressLine2>Near Tarini Temple</addressLine2>
+  <city>Cutack</city>
+  <pin>751784</pin>
+  <landMark>Tarini Temple</landMark>
+</com.saleit.domains.Address>', '9865874512', 'rambofastfood@gmail.com','Food','101');
+
+INSERT INTO rtl_shop (shop_id, shop_name, shop_address, contact_number, email_address,shop_type,shop_owner_id)
+VALUES ('10003', 'Sri Grocery', '<com.saleit.domains.Address>
+  <addressLine1>Tamrit Colony</addressLine1>
+  <addressLine2>Near Ramco Colege</addressLine2>
+  <city>Angul</city>
+  <pin>759122</pin>
+  <landMark>Ramco Colege</landMark>
+</com.saleit.domains.Address>', '9875623145', 'srigrocery@gmail.com','Grocery','102');
+
+INSERT INTO rtl_shop (shop_id, shop_name, shop_address, contact_number, email_address,shop_type,shop_owner_id)
+VALUES ('10004', 'Behera Veriety Store', '<com.saleit.domains.Address>
+  <addressLine1>Railway Colony</addressLine1>
+  <addressLine2>Near Railway Station</addressLine2>
+  <city>Dhenkanal</city>
+  <pin>759253</pin>
+  <landMark>Railway Station</landMark>
+</com.saleit.domains.Address>', '9658745125', 'beheraveriety@gmail.com','Veriety','103');
+
+INSERT INTO rtl_shop (shop_id, shop_name, shop_address, contact_number, email_address,shop_type,shop_owner_id)
+VALUES ('10005', 'All Stop Store', '<com.saleit.domains.Address>
+  <addressLine1>Nandan Vihar</addressLine1>
+  <addressLine2>Near Subhadra Appartment</addressLine2>
+  <city>Bhubaneswar</city>
+  <pin>751024</pin>
+  <landMark>Subhadra Appartment</landMark>
+</com.saleit.domains.Address>', '9865874512', 'allstop@gmail.com','Veriety','104');
+
+
+
+
+
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300001', 'Egg Roll', 'Egg Roll', '45.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300002', 'Egg Chicke Roll', 'Egg Chicke Roll', '55.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300003', 'Veg Roll', 'Veg Roll', '35.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300004', 'Paneer Roll', 'Paneer Roll', '40.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300005', 'Mushroom Roll', 'Mushroom Roll', '40.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300006', 'Veg Noodles', 'Veg Noodles', '35.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300007', 'Egg Noodles', 'Egg Noodles', '40.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300008', 'Egg Chicekn Noodles', 'Egg Chicekn Noodles', '50.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300009', 'Mix Noodles', 'Mix Noodles', '55.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300010', 'Veg Fried Rice', 'Veg Fried Rice', '40.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300011', 'Egg Fried Rice', 'Eggs Fried Rice', '45.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300012', 'Egg Chicken Fried Rice', 'Egg Chicken Fried Rice', '45.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300013', 'Mix Fried Rice', 'Mix Fried Rice', '55.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300014', 'Chilly Chicken', 'Chilly Chicken', '50.0', 'Y','NA','10001');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300015', 'Chicken 65', 'Chicken 65', '55.0', 'Y','NA','10001');
+
+
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300016', 'Veg Soup', 'Veg Soup', '30.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300017', 'Veg Manchow Soup', 'Veg Manchow Soup', '35.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300018', 'Chicken Soup', 'Chicken Soup', '40.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300019', 'Chicken Manchow Soup', 'Chicken Manchow Soup', '45.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300020', 'Veg Sweet Corn Soup', 'Veg Sweet Corn Soup', '40.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300021', 'Chicken Sweet Corn Soup', 'Chicken Sweet Corn Soup', '45.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300022', 'Veg Manchurian', 'Veg Manchurian', '35.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300023', 'Chicken Manchurian', 'Chicken Manchurian', '45.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300024', 'Tandoori Kabab', 'Tandoori Kabab', '150.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300025', 'Resmi Kabab', 'Resmi Kabab', '150.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300026', 'Tangdi Kabab', 'Tangdi Kabab', '150.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300027', 'Chicken Lolipop', 'Chicken Lolipop', '80.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300028', 'Chicken Biriyani', 'Chicken Biriyani', '100.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300029', 'Veg Biriyani', 'Veg Biriyani', '55.0', 'Y','NA','10002');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300030', 'Crispy Chicken', 'Crispy Chicken', '65.0', 'Y','NA','10002');
+
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300031', 'Toor Dal', 'Toor Dal', '80.0', 'Y','NA','10003');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300032', 'Masoor Dal', 'Masoor Dal', '85.0', 'Y','NA','10003');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300033', 'Tata Salt', 'Tata Salt', '40.0', 'Y','NA','10003');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300034', 'Relience Salt', 'Relience Salt', '35.0', 'Y','NA','10003');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300035', 'Chat Masala', 'Chat Masala', '45.0', 'Y','NA','10003');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300036', 'Omfed Milk', 'Omfed Milk', '40.0', 'Y','NA','10003');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300037', 'Pagati Milk', 'Pagati Milk', '45.0', 'Y','NA','10003');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300038', 'Berry Bread', 'Berry Bread', '40.0', 'Y','NA','10003');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300039', 'Ruchi Mixture', 'Ruchi Mixture', '30.0', 'Y','NA','10003');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300040', 'Dawat Rice', 'Dawat Rice', '100.0', 'Y','NA','10003');
+
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300041', 'Soap Case', 'Soap Case', '10.0', 'Y','NA','10004');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300042', 'Dove Sampoo', 'Dove Sampoo 250ml', '100.0', 'Y','NA','10004');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300043', 'Nyle Sampoo', 'Nyle Sampoo 250ml', '120.0', 'Y','NA','10004');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300044', 'Park Avenue Cool Blue Soap', 'Park Avenue Cool Blue Soap 100grm', '35.0', 'Y','NA','10004');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300045', 'Chinthol Cool Lime', 'Chinthol Cool Lime 100grm', '35.0', 'Y','NA','10004');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300046', 'Fogg Deo', 'Fogg Deo 250ml', '250.0', 'Y','NA','10004');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300047', 'Nivia Deo', 'Nivia Deo For Men 250ml', '180.0', 'Y','NA','10004');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300048', 'Fair N Lovely For Men', 'Fair N Lovely For Men 100grm', '120.0', 'Y','NA','10004');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300049', 'Harpic', 'Harpic 250ml', '45.0', 'Y','NA','10004');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300050', 'Scrotch Bright', 'Scrotch Bright', '15.0', 'Y','NA','10004');
+
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300051', 'Garnier Acno Fight', 'Garnier Acno Fight 250ml', '180.0', 'Y','NA','10005');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300052', 'Garnier Oil Control', 'Garnier Oil Control', '180.0', 'Y','NA','10005');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300053', 'Lakme Anti Aging', 'Lakme Anti Aging 250ml', '300.0', 'Y','NA','10005');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300054', 'Olay Face Cream', 'Olay Face Cream 250ml', '650.0', 'Y','NA','10005');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300055', 'Surf', 'Surf 100grm', '10.0', 'Y','NA','10005');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300056', 'Ariel', 'Ariel 200grm', '20.0', 'Y','NA','10005');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300057', 'Lakme Eye Liner', 'Lakme Eye Liner', '180.0', 'Y','NA','10005');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300058', 'Mac Lipstick', 'Mac Lipstick', '1200.0', 'Y','NA','10005');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300059', 'Lakme Sun Screen Lotion', 'Lakme Sun Screen Lotion', '150.0', 'Y','NA','10004');
+INSERT INTO rtl_item (item_id, item_name, item_description, item_price, availability,item_image_id,shop_id)
+VALUES ('300060', 'Yardely Deo', 'Yardely Deo 250ml', '165.0', 'Y','NA','10005');
+
+
+
+
